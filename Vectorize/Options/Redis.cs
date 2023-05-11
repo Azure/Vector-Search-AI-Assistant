352d130026f28efe8ed9c56ef7503616604338f8
Mark Brown <mjbrown@microsoft.com>
2023-05-11 07:46:31 -0700

    Add DI to Redis

---
+++ Vectorize/Options/Redis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCopilot.Vectorize.Options
+{
+    public class Redis
+    {
+        public string? ConnectionString { get; set; }
+    }
+}
