9e4f8f3f263cbc7901f9e7f45f6e06d18b6f287c
Mark Brown <mjbrown@microsoft.com>
2023-05-10 18:05:19 -0700

    mongo

---
+++ Vectorize/Models/BsonBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace Vectorize.Models
+{
+    public class BsonBase
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+    }
+
+    
+
+}
