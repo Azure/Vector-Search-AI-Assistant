1ad99541a001f5008d74e6ff35567cd9ce1f6b61
Mark Brown <mjbrown@microsoft.com>
2023-05-17 06:09:34 -0700

    Add Remove Products Trigger

---
+++ Vectorize/AddRemoveData.cs
@@ -0,0 +1,162 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Cosmos;
+using DataCopilot.Vectorize.Models;
+using DataCopilot.Vectorize.Services;
+using System.ComponentModel;
+using Container = Microsoft.Azure.Cosmos.Container;
+
+namespace Datacopilot.Vectorize
+{
+    public class AddRemoveData
+    {
+
+        private readonly RedisService _redis;
+
+        public AddRemoveData(RedisService redis)
+        {
+            _redis = redis;
+        }
+
+
+        [FunctionName("AddRemoveData")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+                Connection = "CosmosDBConnection")]CosmosClient cosmosClient,
+            ILogger logger)
+        {
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string? action = req.Query["action"];
+
+            try
+            {
+
+                if (action == "add")
+                {
+                    await AddProduct(cosmosClient, logger);
+                }
+                else if (action == "remove")
+                {
+                    await RemoveProduct(cosmosClient, logger);
+
+                }
+                else
+                {
+                    throw new Exception("Bad Request: Missing value for action in query string, add or remove");
+                }
+
+                string responseMessage = "HTTP trigger function executed successfully.";
+
+                return new OkObjectResult(responseMessage);
+            }
+            catch (Exception ex)
+            {
+
+                return new BadRequestObjectResult(ex);
+
+            }
+        }
+
+        public async Task AddProduct(CosmosClient cosmosClient, ILogger logger)
+        {
+
+            try
+            {
+
+                Container container = cosmosClient.GetContainer("database", "product");
+
+                await container.CreateItemAsync(GetCosmicSock);
+
+                logger.LogInformation("Added Cosmic Sock to product");
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                throw;
+
+            }
+
+        }
+
+        public async Task RemoveProduct(CosmosClient cosmosClient, ILogger logger)
+        {
+
+            try
+            {
+
+                Container embeddingContainer = cosmosClient.GetContainer("database", "embedding");
+                Container productContainer = cosmosClient.GetContainer("database", "product");
+
+
+                //Delete from Cosmos product container
+                await productContainer.DeleteItemAsync<Product>(id: GetCosmicSock.id, partitionKey: new PartitionKey(GetCosmicSock.categoryId));
+
+                //Lookup DocumentVector Id
+                string documentVectorId = await LookupDocumentVectorId(embeddingContainer, "product", GetCosmicSock.categoryId, GetCosmicSock.id);
+                //Remove from Redis
+                await _redis.RemoveVector(documentVectorId);
+                //Then delete from embedding container
+                await embeddingContainer.DeleteItemAsync<DocumentVector>(id: documentVectorId, partitionKey: new PartitionKey(documentVectorId));
+
+                logger.LogInformation("Removed Cosmic Sock from product");
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                throw;
+
+            }
+        }
+
+        public Product GetCosmicSock
+        {
+            get => new Product(
+                id: "00001",
+                categoryId: "C48B4EF4-D352-4CD2-BCB8-CE89B7DFA642",
+                categoryName: "Clothing, Socks",
+                sku: "SO-R999-M",
+                name: "Cosmic Racing Socks, M",
+                description: "The product called Cosmic Racing Socks, M",
+                price: 6.00,
+                tags: new List<Tag>
+                {
+                    new Tag(id: "51CD93BF-098C-4C25-9829-4AD42046D038", name: "Tag-25"),
+                    new Tag(id: "5D24B427-1402-49DE-B79B-5A7013579FBC", name: "Tag-76"),
+                    new Tag(id: "D4EC9C09-75F3-4ADD-A6EB-ACDD12C648FA", name: "Tag-153")
+                });
+        }
+
+        public async Task<string> LookupDocumentVectorId(Container embeddingContainer, string containerName, string partitionKey, string itemId)
+        {
+
+            string documentVectorId = string.Empty;
+
+            using var feedIterator = embeddingContainer.GetItemQueryIterator<DocumentVector>(
+                new QueryDefinition("SELECT * FROM c WHERE c.containerName = @containerName AND c.partitionKey = @partitionKey AND c.itemId = @itemId")
+                .WithParameter("@containerName", containerName)
+                .WithParameter("@partitionKey", partitionKey)
+                .WithParameter("@itemId", itemId));
+
+
+            while (feedIterator.HasMoreResults)
+            {
+                
+                var response = await feedIterator.ReadNextAsync();
+
+                foreach (var item in response)
+                {
+                    documentVectorId = item.id;
+                }
+            }
+
+            return documentVectorId;
+        }
+    }
+}
--- Vectorize/Models/Customer.cs
+++ Vectorize/Models/Customer.cs
@@ -2,6 +2,7 @@
 {
     public class Customer
     {
+        
         public string id { get; set; }
         public string type { get; set; }
         public string customerId { get; set; }
@@ -14,12 +15,37 @@
         public List<CustomerAddress> addresses { get; set; }
         public Password password { get; set; }
         public int salesOrderCount { get; set; }
+
+        public Customer(string id, string type, string customerId, string title,
+            string firstName, string lastName, string emailAddress, string phoneNumber,
+            string creationDate, List<CustomerAddress> addresses, Password password,
+            int salesOrderCount)
+        {
+            this.id = id;
+            this.type = type;
+            this.customerId = customerId;
+            this.title = title;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            this.phoneNumber = phoneNumber;
+            this.creationDate = creationDate;
+            this.addresses = addresses;
+            this.password = password;
+            this.salesOrderCount = salesOrderCount;
+        }
     }
 
     public class Password
     {
         public string hash { get; set; }
         public string salt { get; set; }
+
+        public Password(string hash, string salt)
+        {
+            this.hash = hash;
+            this.salt = salt;
+        }
     }
 
     public class CustomerAddress
@@ -31,11 +57,28 @@
         public string country { get; set; }
         public string zipCode { get; set; }
         public Location location { get; set; }
+
+        public CustomerAddress(string addressLine1, string addressLine2, string city, string state, string country, string zipCode, Location location)
+        {
+            this.addressLine1 = addressLine1;
+            this.addressLine2 = addressLine2;
+            this.city = city;
+            this.state = state;
+            this.country = country;
+            this.zipCode = zipCode;
+            this.location = location;
+        }
     }
 
     public class Location
     {
         public string type { get; set; }
         public List<float> coordinates { get; set; }
+
+        public Location(string type, List<float> coordinates)
+        {
+            this.type = type;
+            this.coordinates = coordinates;
+        }
     }
 }
