17c16209654e86d0b825cba3f8f6d81406261404
joelhulen <joelhulen@gmail.com>
2023-06-05 11:08:14 -0400

    Renamed the concreted class implementation of the IVectorDatabase* interfaces for our Cognitive Search example

--- Search/Program.cs
+++ Search/Program.cs
@@ -83,7 +83,7 @@ static class ProgramExtensions
                 );
             }
         });
-        services.AddSingleton<IVectorDatabaseServiceQueries, VectorDatabaseService>((provider) =>
+        services.AddSingleton<IVectorDatabaseServiceQueries, CognitiveSearchService>((provider) =>
         {
             var cognitiveSearchOptions = provider.GetRequiredService<IOptions<CognitiveSearch>>();
 
@@ -93,7 +93,7 @@ static class ProgramExtensions
             }
             else
             {
-                return new VectorDatabaseService
+                return new CognitiveSearchService
                 (
                     azureSearchAdminKey: cognitiveSearchOptions.Value?.AdminKey ?? string.Empty,
                     azureSearchServiceEndpoint: cognitiveSearchOptions.Value?.Endpoint ?? string.Empty,
