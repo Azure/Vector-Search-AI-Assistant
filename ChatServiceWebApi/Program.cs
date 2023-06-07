f0a2dada5f25b16fdc4c4f3fe402abce9f6991ec
joelhulen <joelhulen@gmail.com>
2023-06-07 19:17:18 -0400

    Cleanup

--- ChatServiceWebApi/Program.cs
+++ ChatServiceWebApi/Program.cs
@@ -110,26 +110,6 @@ namespace ChatServiceWebApi
                 var service = scope.ServiceProvider.GetService<ChatEndpoints>();
                 service?.Map(app);
             }
-            
-            //var summaries = new[]
-            //{
-            //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            //};
-
-            //app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            //{
-            //    var forecast = Enumerable.Range(1, 5).Select(index =>
-            //        new WeatherForecast
-            //        {
-            //            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            //            TemperatureC = Random.Shared.Next(-20, 55),
-            //            Summary = summaries[Random.Shared.Next(summaries.Length)]
-            //        })
-            //        .ToArray();
-            //    return forecast;
-            //})
-            //.WithName("GetWeatherForecast")
-            //.WithOpenApi();
 
             app.Run();
         }
