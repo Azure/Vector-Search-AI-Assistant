1f0a1f766943f93a82fc897f6f117ad02d446b1e
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2023-08-24 09:25:55 -0500

    Explicitly setting TPM for OpenAI deployments

--- scripts/Deploy-OpenAi.ps1
+++ scripts/Deploy-OpenAi.ps1
@@ -19,14 +19,14 @@ $deployments = (az cognitiveservices account deployment list -g $resourceGroup -
 Write-Host "Existing deployments: $($deployments)"
 if (-Not ($deployments -Contains $completionsDeployment)) {
     Write-Host("The Azure OpenAI deployment $($completionsDeployment) under account $($name) was not found, creating it...")
-    az cognitiveservices account deployment create -g $resourceGroup -n $name --deployment-name $completionsDeployment --model-name 'gpt-35-turbo' --model-version '0301' --model-format OpenAI
+    az cognitiveservices account deployment create -g $resourceGroup -n $name --deployment-name $completionsDeployment --model-name 'gpt-35-turbo' --model-version '0301' --model-format OpenAI --capacity 240
 }
 
 $deployments = (az cognitiveservices account deployment list -g $resourceGroup -n $name --query '[].name' -o json | ConvertFrom-Json)
 Write-Host "Existing deployments: $($deployments)"
 if (-Not ($deployments -Contains $embeddingsDeployment)) {
     Write-Host("The Azure OpenAI deployment $($embeddingsDeployment) under account $($name) was not found, creating it...")
-    az cognitiveservices account deployment create -g $resourceGroup -n $name --deployment-name $embeddingsDeployment --model-name 'text-embedding-ada-002' --model-version '2' --model-format OpenAI
+    az cognitiveservices account deployment create -g $resourceGroup -n $name --deployment-name $embeddingsDeployment --model-name 'text-embedding-ada-002' --model-version '2' --model-format OpenAI --capacity 240
 }
 
 Pop-Location
