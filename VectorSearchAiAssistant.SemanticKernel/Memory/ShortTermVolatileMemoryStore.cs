33cdbdc5871fb70045b95f81a32b2bcc7d8796ce
Mark Brown <mjbrown@microsoft.com>
2023-10-14 14:38:14 -0700

    updates and fixes

--- VectorSearchAiAssistant.SemanticKernel/Memory/ShortTermVolatileMemoryStore.cs
+++ VectorSearchAiAssistant.SemanticKernel/Memory/ShortTermVolatileMemoryStore.cs
@@ -33,6 +33,11 @@ namespace VectorSearchAiAssistant.SemanticKernel.Memory
             if (!await DoesCollectionExistAsync(_collectionName))
                 await CreateCollectionAsync(_collectionName);
 
+
+            //Upsert the memories that were generated from faceted queries in Cog Search into SK volatile memory.
+            //These were generated using ACSMemorySourceConfig.json in blob storage that was used to execute faceted queries in Cog Search to iterate through
+            //each product category stored and count up the number of products in each category. The query also counts all the products for the entire company.
+            //The content here has embeddings generated on it so it can be used in a vector query by the user.
             foreach (var memory in _memories)
             {
                 var itemToEmbed = EmbeddingUtility.Transform(memory);
