55a5f8cf71f8414266e7ed44e5f26911a22e8941
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2024-03-12 13:17:22 -0500

    Adding updated k8s manifests for AZD deployment

--- aks/infra/azd-hooks/predeploy.ps1
+++ aks/infra/azd-hooks/predeploy.ps1
@@ -1,5 +1,5 @@
 Write-Host "Retrieving cluster credentials"
-az aks get-credentials --resource-group ${AZURE_RESOURCE_GROUP} --name ${AZURE_AKS_CLUSTER_NAME}
+az aks get-credentials --resource-group ${env:AZURE_RESOURCE_GROUP} --name ${env:AZURE_AKS_CLUSTER_NAME}
 
 # Add required Helm repos
 helm repo add aso2 https://raw.githubusercontent.com/Azure/azure-service-operator/main/v2/charts
@@ -19,15 +19,16 @@ Write-Host "Checking if Azure Service Operator is installed"
 $aso2Status=$(helm status aso2 -n azureserviceoperator-system 2>&1)
 if ($aso2Status -like "Error: release: not found") {
      Write-Host "Azure Service Operator not installed, installing"
-     #kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.8.2/cert-manager.yaml
+     kubectl apply -f ./manifests/aso_crd_v2.6.0.yaml
 
      helm upgrade --install --devel aso2 aso2/azure-service-operator `
           --create-namespace `
           --namespace=azureserviceoperator-system `
-          --set azureSubscriptionID=${AZURE_SUBSCRIPTION_ID} `
-          --set azureTenantID=${AZURE_TENANT_ID} `
-          --set azureClientID=${ASO_WORKLOADIDENTITY_CLIENT_ID} `
-          --set useWorkloadIdentityAuth=true
+          --set azureSubscriptionID=${env:AZURE_SUBSCRIPTION_ID} `
+          --set azureTenantID=${env:AZURE_TENANT_ID} `
+          --set azureClientID=${env:ASO_WORKLOADIDENTITY_CLIENT_ID} `
+          --set useWorkloadIdentityAuth=true `
+          --version 2.6.0
 } else {
   Write-Host "Azure Service Operator installed, skipping"
 }
@@ -43,3 +44,51 @@ if ($kedatatus -like "Error: release: not found") {
 } else {
   Write-Host "KEDA installed, skipping"
 }
+
+Write-Host "Retrieving Cognitive Search Key"
+$AZURE_COGNITIVE_SEARCH_KEY=$(
+  az search admin-key show `
+    --service-name ${env:AZURE_COGNITIVE_SEARCH_NAME} `
+    --resource-group ${env:AZURE_RESOURCE_GROUP} `
+    -o json | ConvertFrom-Json).primaryKey
+
+azd env set AZURE_COGNITIVE_SEARCH_KEY ${AZURE_COGNITIVE_SEARCH_KEY}
+
+Write-Host "Retrieving OpenAI Key"
+$AZURE_OPENAI_KEY=$(
+  az cognitiveservices account keys list `
+    --name ${env:AZURE_OPENAI_NAME} `
+    --resource-group ${env:AZURE_RESOURCE_GROUP} `
+    -o json | ConvertFrom-Json).key1
+
+azd env set AZURE_OPENAI_KEY ${AZURE_OPENAI_KEY}
+
+Write-Host "Retrieving CosmosDB Key"
+$AZURE_COSMOS_DB_KEY=$(
+  az cosmosdb keys list `
+    --name ${env:AZURE_COSMOS_DB_NAME} `
+    --resource-group ${env:AZURE_RESOURCE_GROUP} `
+    -o json | ConvertFrom-Json).primaryMasterKey
+
+azd env set AZURE_COSMOS_DB_KEY ${AZURE_COSMOS_DB_KEY}
+
+Write-Host "Retrieving Storage Connection String"
+$AZURE_STORAGE_KEY=$(
+  az storage account keys list `
+    --account-name ${env:AZURE_STORAGE_ACCOUNT_NAME} `
+    --resource-group ${env:AZURE_RESOURCE_GROUP} `
+    -o json | ConvertFrom-Json)[0].value
+
+$AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=${env:AZURE_STORAGE_ACCOUNT_NAME};AccountKey=${AZURE_STORAGE_KEY};EndpointSuffix=core.windows.net"
+
+azd env set AZURE_STORAGE_CONNECTION_STRING ${AZURE_STORAGE_CONNECTION_STRING}
+
+az storage container create --account-name ${env:AZURE_STORAGE_ACCOUNT_NAME} --name "system-prompt" --only-show-errors
+az storage azcopy blob upload -c system-prompt --account-name ${env:AZURE_STORAGE_ACCOUNT_NAME} -s "../SystemPrompts/*" --recursive --only-show-errors
+
+az storage container create --account-name ${env:AZURE_STORAGE_ACCOUNT_NAME} --name "memory-source" --only-show-errors
+az storage azcopy blob upload -c memory-source --account-name ${env:AZURE_STORAGE_ACCOUNT_NAME} -s "../MemorySources/*.json" --recursive --only-show-errors
+
+az storage container create --account-name ${env:AZURE_STORAGE_ACCOUNT_NAME} --name "product-policy" --only-show-errors
+az storage azcopy blob upload -c product-policy --account-name ${env:AZURE_STORAGE_ACCOUNT_NAME} -s "../MemorySources/*.txt" --recursive --only-show-errors
+
