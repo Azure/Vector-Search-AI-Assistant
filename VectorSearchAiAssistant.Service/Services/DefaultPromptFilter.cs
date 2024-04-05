79f3df0016df9256c720ca9feec5441337b92dc5
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2024-04-05 17:03:10 +0300

    Use prompt filters for observability

---
+++ VectorSearchAiAssistant.Service/Services/DefaultPromptFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SemanticKernel;
+
+#pragma warning disable SKEXP0001
+
+namespace VectorSearchAiAssistant.Service.Services
+{
+    public class DefaultPromptFilter : IPromptFilter
+    {
+        public string RenderedPrompt => _renderedPrompt;
+
+        private string _renderedPrompt = string.Empty;
+
+        public void OnPromptRendered(PromptRenderedContext context)
+        {
+            _renderedPrompt = context.RenderedPrompt;
+        }
+
+        public void OnPromptRendering(PromptRenderingContext context)
+        {
+        }
+    }
+}
